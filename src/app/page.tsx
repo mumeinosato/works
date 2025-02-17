c704fe0408564fb0c88f5f41684524dd2a509d41
mumeinosato <66110797+mumeinosato@users.noreply.github.com>
2025-02-17 21:32:57 +0900

    プロジェクト紹介を追加し、Dockerfileを作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
 "use client";
 
 import '@ant-design/v5-patch-for-react-19';
-import { Button, Card, Flex, Modal, Switch, version } from 'antd';
+import { Card } from 'antd';
 import * as React from 'react';
-import { useState, useEffect } from 'react';
 
 const projects = [
+  {
+    name: '制作物紹介サイト',
+    description: 'このサイトです。',
+    github: 'https://github.com/mumeinosato/works',
+    demo: 'https://works.mtayo.net/',
+  },
+  {
+    name: 'Feedo',
+    description: '旧バージョンをリニューアルしたものです。',
+    github: 'https://github.com/cube5963/feedo_front',
+    demo: 'https://e55c631a-45d9-4671-7295-38c7f9b35eca.mtayo.net/',
+  },
   {
     name: 'Discord TTS Bot',
     description: 'VOICEVOX Engineを使ったDiscordのテキスト読み上げBotです。',
@@ -28,20 +39,22 @@ const projects = [
     description: 'Androidで動く電車の時刻表アプリです。学校の最寄り駅の時刻表を調べる時間を短縮するために作成しました。',
     github: 'https://github.com/mumeinosato/timetable',
     demo: '',
+  },
+  {
+    name: 'Feedo（旧バージョン）',
+    description: '同級生がStartUpWeekend静岡7thに参加した際に制作支援したアプリです。',
+    github: 'https://github.com/cube5963/feedo_old',
+    demo: 'https://ca55bad4-a542-2735-89ab-670a991e0c24.mtayo.net/',
+  },
+  {
+    name: 'しかのこのこのここしたんたん',
+    description: 'マルコフ連鎖を使用し「しかのこのこのここしたんたん」を生成します。',
+    github: 'https://github.com/mumeinosato/shikanoko-nokonoko-koshitantan',
+    demo: '',
   }
 ];
 
 const App = () => {
-  const [darkMode, setDarkMode] = useState(false);
-
-  useEffect(() => {
-    if (darkMode) {
-      document.documentElement.classList.add('dark');
-    } else {
-      document.documentElement.classList.remove('dark');
-    }
-  }, [darkMode]);
-
   return (
     <div className="min-h-screen bg-gradient-to-r from-gray-100 to-gray-300 p-8 transition-colors duration-500">
       <header className="text-center mb-12">
@@ -49,17 +62,35 @@ const App = () => {
       </header>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {projects.map((project) => (
-          <Card key={project.name} title={project.name} className="shadow-2xl transform transition duration-500 hover:scale-105 bg-white dark:bg-gray-800 bg-opacity-90">
-            <p className="text-gray-700 dark:text-gray-300 mb-4">{project.description}</p>
-            <a href={project.github} target="_blank" rel="noopener noreferrer" className="text-indigo-500 hover:underline">
-              GitHub Repository
-            </a>
-            <br />
-            {project.demo && (
-              <a href={project.demo} target="_blank" rel="noopener noreferrer" className="text-indigo-500 hover:underline">
-                Demo Page
-              </a>
-            )}
+          <Card
+            key={project.name}
+            title={project.name}
+            className="shadow-2xl transform transition duration-500 hover:scale-105 bg-white dark:bg-gray-800 bg-opacity-90"
+          >
+            <div className="flex flex-col h-full">
+              <p className="text-gray-700 dark:text-gray-300 mb-4">{project.description}</p>
+                <div className="mt-auto">
+                <a
+                  href={project.github}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-indigo-500 hover:underline"
+                >
+                  GitHub Repository
+                </a>
+                <br />
+                {project.demo && (
+                  <a
+                  href={project.demo}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-indigo-500 hover:underline"
+                  >
+                  Demo Page
+                  </a>
+                )}
+                </div>
+            </div>
           </Card>
         ))}
       </div>
